a9bd26e34092e18a53b2b936f3b270f481f96345
alfredocassanj-hub <alfredocassanj@gmail.com>
2026-03-29 01:16:54 +0100

    week4

---
+++ lis.py
@@ -0,0 +1,6 @@
+my_list = [1, "apple", 3.14, "banana"]
+print(my_list)
+Pty_list = []   #list , pronto para ser preenchido
+fruits = ["bnana""apple", "orange","mango"]
+numember = [1, 2, 3, 4, 5]
+print = (fruits)
